ad6877bec109294309402c5fdda2dce8045e6fa1
willpasc <williampascal8@gmail.com>
2022-09-30 18:35:46 +0700

    marc update

--- resources/views/partials/frontend/_footer-paul.blade.php
+++ resources/views/partials/frontend/_footer-paul.blade.php
@@ -11,77 +11,115 @@
                 </div>
                 <div class="pt-3" style="border-bottom: 1px solid white; width:100%;"></div>
             </div>
-            <div class="row pt-5">
-                <div class="col-sm-8 col-md-5 col-lg-3 p-b-20">
-                    <div class="size-h-1 flex-s-e p-b-6 m-b-18">
-                        <a href="https://trinitiland.com" class="mx-auto" target="_blank">
-                            <img class="max-s-full" src="{{ asset('images/marc/about/Marcs Website components-19.png') }}" alt="IMG" style="max-width: 200px;">
-                        </a>
-                    </div>
-
-                    <div class="text-center">
-                        <p class="t1-s-2 cl-13 p-b-9">
-                            <span class="custom-font-1">PT PURI TRINITI BATAM</span>
-                        </p>
-                    </div>
+        </div>
+        <div class="container-fluid pt-5">
+            <div class="row no-gutters">
+                <div class="col-md-4 col-12 mb-4 mb-md-0 text-center">
+                    <a href="{{ route('home') }}">
+                        <img class="mb-4" src="{{ asset('images/marc/footer-image-1.png') }}" width="180">
+                    </a>
+                    <br/>
+                    <a href="{{ route('frontend.catalogue.download') }}" class="btn btn-default t1-s-1 font-weight-bold" style="background-color: #fff; color: #000; border-radius: 0;">DOWNLOAD E-CATALOGUE</a>
                 </div>
-
-                <div class="col-sm-8 col-md-5 col-lg-3 p-b-20">
-                    <div class="size-h-1 flex-s-e m-b-18">
-                        <h4 class="t1-m-3  cl-0">
-                            <span class="custom-font-2" style="font-size: 20px;">Living Gallery</span>
-                        </h4>
+                <div class="col-md-8 col-12 text-center text-md-left">
+                    <div class="row no-gutters mb-5 mb-md-3">
+                        <div class="col-md-2 col-12 pr-md-1 mb-3 mb-md-0 text-center text-md-left">
+                            <p class="t1-s-1 cl-13">
+                                <span class="custom-font-1">PT PURI TRINITI BATAM</span>
+                            </p>
+                        </div>
+                        <div class="col-md-3"></div>
+                        <div class="col-md-3 col-12">
+                            <div class="d-none d-md-block pl-md-4">
+                                <a href="https://trinitiland.com" target="_blank">
+                                    <img src="{{ asset('images/marc/about/Marcs Website components-19.png') }}"  style="max-width: 150px; margin-top: -26px;">
+                                </a>
+                            </div>
+                            <div class="d-block d-md-none text-center">
+                                <a href="https://trinitiland.com" target="_blank">
+                                    <img src="{{ asset('images/marc/about/Marcs Website components-19.png') }}"  style="max-width: 150px;">
+                                </a>
+                            </div>
+                        </div>
+                        <div class="col-md col-12"></div>
                     </div>
+                    <div class="row no-gutters text-center text-md-left">
+                        <div class="col-md col-12 pb-3">
+                            <div class=" m-b-18">
+                                <h4 class="t1-m-3 cl-0">
+                                    <span class="custom-font-2" style="font-size: 20px;">Living Gallery</span>
+                                </h4>
+                            </div>
 
-                    <ul>
-                        <li class="flex-wr-s-s t1-s-2 cl-13 p-b-9">
-                            <span class="size-w-4">
+                            <ul>
+                                <li class=" t1-s-2 cl-13 p-b-9">
+                                    <span class="size-w-4">
                                         Grand Batam Mall - 2nd Floor<br/>
                                     Jl. Pembangunan, Batu Selicin,<br/>
                                     Kec. Lubuk Baja, Kota Batam,<br/>
                                     Kepulauan Riau 29444
                                     </span>
-                        </li>
-                    </ul>
-                </div>
 
-                <div class="col-sm-8 col-md-5 col-lg-3 p-b-20">
-                    <div class="size-h-1 flex-s-e m-b-18">
-                        <h4 class="t1-m-3  cl-0">
-                            <span class="custom-font-2" style="font-size: 20px;">Triniti Land HQ</span>
-                        </h4>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="col-md col-12 pb-3">
+                            <div class=" m-b-18">
+                                <h4 class="t1-m-3 cl-0">
+                                    <span class="custom-font-2" style="font-size: 20px;">Marketing Office</span>
+                                </h4>
+                            </div>
+
+                            <ul>
+                                <li class=" t1-s-2 cl-13 p-b-9">
+                                    <span class="size-w-4">
+                                        Komp. Ruko Batamas<br/>
+                                        Blok A No.1, <br/>
+                                        Pasir Putih - Batam Centre
+                                    </span>
+
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="col-md col-12 pb-3">
+                            <div class=" m-b-18">
+                                <h4 class="t1-m-3 cl-0">
+                                    <span class="custom-font-2" style="font-size: 20px;">Triniti Land HQ</span>
+                                </h4>
+                            </div>
+
+                            <ul>
+                                <li class=" t1-s-2 cl-13 p-b-9">
+                                    <span class="size-w-4">
+                                        District 8 SCBD, Prosperity Tower,<br/>
+                                        Lantai 18A/J, Jl. Senopati Raya, <br/>
+                                        Senayan, Jakarta Selatan,<br/>
+                                        Indonesia
+                                    </span>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="col-md-4 col-12 pb-3">
+                            <div class=" m-b-18">
+                                <h4 class="t1-m-3 cl-0">
+                                    <span class="custom-font-2" style="font-size: 20px;">Contact</span>
+                                </h4>
+                            </div>
+
+                            <ul>
+                                <li class=" t1-s-2 cl-13 p-b-9">
+                                    <span class="size-w-4">
+                                       (+62) 812 7561 9998<br/>
+                                       (+62) 853 4528 9988<br/>
+                                        info@trinitiland.com<br/>
+                                        www.trinitiland.com
+                                    </span>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
-
-                    <ul>
-                        <li class="flex-wr-s-s t1-s-2 cl-13 p-b-9">
-                            <span class="size-w-4">
-                                District 8 SCBD, Prosperity Tower,<br/>
-                                Lantai 18A/J, Jl. Senopati Raya, <br/>
-                                Senayan, Jakarta Selatan,<br/>
-                                Indonesia
-                            </span>
-                        </li>
-                    </ul>
                 </div>
 
-                <div class="col-sm-8 col-md-5 col-lg-3 p-b-20">
-                    <div class="size-h-1 flex-s-e m-b-18">
-                        <h4 class="t1-m-3  cl-0">
-                            <span class="custom-font-2" style="font-size: 20px;">Contact</span>
-                        </h4>
-                    </div>
-
-                    <ul>
-                        <li class="flex-wr-s-s t1-s-2 cl-13 p-b-9">
-                            <span class="size-w-4">
-                               (+62) 812 7561 9998<br/>
-                               (+62) 853 4528 9988<br/>
-                                info@trinitiland.com<br/>
-                                www.trinitiland.com
-                            </span>
-                        </li>
-                    </ul>
-                </div>
             </div>
         </div>
     </div>
--- resources/views/partials/frontend/_footer.blade.php
+++ resources/views/partials/frontend/_footer.blade.php
@@ -1,8 +1,8 @@
 <!-- Footer -->
 <footer>
     <div class="parallax100 pt-3 pt-md-5 p-b-15 bg-1" style="background: #1f2020;">
-        <div class="container p-5 p-md-2">
-            <div class="row">
+        <div class="container-fluid p-5 p-md-2">
+            <div class="row no-gutters">
                 <div class="col-md-4 col-12 mb-4 mb-md-0 text-center">
                     <a href="{{ route('home') }}">
                         <img class="mb-4" src="{{ asset('images/marc/footer-image-1.png') }}" width="180">
@@ -10,15 +10,16 @@
                     <br/>
                     <a href="{{ route('frontend.catalogue.download') }}" class="btn btn-default t1-s-1 font-weight-bold" style="background-color: #fff; color: #000; border-radius: 0;">DOWNLOAD E-CATALOGUE</a>
                 </div>
-                <div class="col-md-8 col-12">
-                    <div class="row mb-5 mb-md-3">
-                        <div class="col-md-4 col-12 pr-md-1 mb-3 mb-md-0 text-center text-md-left">
+                <div class="col-md-8 col-12 text-center text-md-left">
+                    <div class="row no-gutters mb-5 mb-md-3">
+                        <div class="col-md-2 col-12 pr-md-1 mb-3 mb-md-0 text-center text-md-left">
                             <p class="t1-s-1 cl-13">
                                 <span class="custom-font-1">PT PURI TRINITI BATAM</span>
                             </p>
                         </div>
-                        <div class="col-md-4 col-12">
-                            <div class="d-none d-md-block">
+                        <div class="col-md-3"></div>
+                        <div class="col-md-3 col-12">
+                            <div class="d-none d-md-block pl-md-4">
                                 <a href="https://trinitiland.com" target="_blank">
                                     <img src="{{ asset('images/marc/about/Marcs Website components-19.png') }}"  style="max-width: 150px; margin-top: -26px;">
                                 </a>
@@ -29,18 +30,18 @@
                                 </a>
                             </div>
                         </div>
-                        <div class="col-md-4 col-12"></div>
+                        <div class="col-md col-12"></div>
                     </div>
-                    <div class="row">
-                        <div class="col-md-4 col-12">
-                            <div class="flex-s-e m-b-18">
+                    <div class="row no-gutters text-center text-md-left">
+                        <div class="col-md col-12 pb-3">
+                            <div class=" m-b-18">
                                 <h4 class="t1-m-3 cl-0">
                                     <span class="custom-font-2" style="font-size: 20px;">Living Gallery</span>
                                 </h4>
                             </div>
 
                             <ul>
-                                <li class="flex-wr-s-s t1-s-2 cl-13 p-b-9">
+                                <li class=" t1-s-2 cl-13 p-b-9">
                                     <span class="size-w-4">
                                         Grand Batam Mall - 2nd Floor<br/>
                                     Jl. Pembangunan, Batu Selicin,<br/>
@@ -51,15 +52,33 @@
                                 </li>
                             </ul>
                         </div>
-                        <div class="col-md-4 col-12">
-                            <div class="flex-s-e m-b-18">
+                        <div class="col-md col-12 pb-3">
+                            <div class=" m-b-18">
+                                <h4 class="t1-m-3 cl-0">
+                                    <span class="custom-font-2" style="font-size: 20px;">Marketing Office</span>
+                                </h4>
+                            </div>
+
+                            <ul>
+                                <li class=" t1-s-2 cl-13 p-b-9">
+                                    <span class="size-w-4">
+                                        Komp. Ruko Batamas<br/>
+                                        Blok A No.1, <br/>
+                                        Pasir Putih - Batam Centre
+                                    </span>
+
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="col-md col-12 pb-3">
+                            <div class=" m-b-18">
                                 <h4 class="t1-m-3 cl-0">
                                     <span class="custom-font-2" style="font-size: 20px;">Triniti Land HQ</span>
                                 </h4>
                             </div>
 
                             <ul>
-                                <li class="flex-wr-s-s t1-s-2 cl-13 p-b-9">
+                                <li class=" t1-s-2 cl-13 p-b-9">
                                     <span class="size-w-4">
                                         District 8 SCBD, Prosperity Tower,<br/>
                                         Lantai 18A/J, Jl. Senopati Raya, <br/>
@@ -69,15 +88,15 @@
                                 </li>
                             </ul>
                         </div>
-                        <div class="col-md-4 col-12">
-                            <div class="flex-s-e m-b-18">
+                        <div class="col-md-4 col-12 pb-3">
+                            <div class=" m-b-18">
                                 <h4 class="t1-m-3 cl-0">
                                     <span class="custom-font-2" style="font-size: 20px;">Contact</span>
                                 </h4>
                             </div>
 
                             <ul>
-                                <li class="flex-wr-s-s t1-s-2 cl-13 p-b-9">
+                                <li class=" t1-s-2 cl-13 p-b-9">
                                     <span class="size-w-4">
                                        (+62) 812 7561 9998<br/>
                                        (+62) 853 4528 9988<br/>
